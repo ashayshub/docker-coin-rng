350fc17d5ff6950e0cf465e3f055b536eccc790b
Muhammad Ahsan Ali <ahsan@flux7.com>
2016-11-04 13:30:01 +0500

    adding unit tests

---
+++ tests/test_rng.py
@@ -0,0 +1,18 @@
+import os
+import tempfile
+import pytest
+from rng import rng
+
+
+@pytest.fixture
+def client(request):
+    rng.app.config['TESTING'] = True
+    client = rng.app.test_client()
+    return client
+
+def test_rng(client):
+    """Start with a blank database."""
+    number_of_bytes = 32
+    rv = client.get('/%s'%number_of_bytes)
+    assert len(rv.data) == number_of_bytes
+
