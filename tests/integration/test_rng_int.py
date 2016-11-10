26b88220814e731cca3eefca9386727bb834597d
Muhammad Ahsan Ali <ahsan@flux7.com>
2016-11-10 15:28:43 +0500

    modifying int tests

--- tests/integration/test_rng_int.py
+++ tests/integration/test_rng_int.py
@@ -5,5 +5,6 @@ def test_index(base_url):
     assert 200 == urllib2.urlopen('{0}/'.format(base_url)).getcode()
 
 def test_random_bytes(base_url):
+	"Make sure the number of bytes returned are correct"
     bytes = urllib2.urlopen('{0}/32'.format(base_url)).read()
     assert 32 == len(bytes)
